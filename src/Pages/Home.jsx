017ad64578ca9a668ba9ff02be189e29d8633b76
Pradhansumit <pradhansumit026@gmail.com>
2025-07-15 19:05:30 +0530

    Minor change in head and home component.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,20 @@
 const Home = () => {
-  return (
-    <div className="min-h-[calc(100vh-27vh)] flex items-center justify-center">
-      <div>
-        <img src="/signature.svg" alt="Signature" className="invert" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="min-h-[calc(100vh-27vh)] flex items-center justify-center">
+            <div className="flex flex-col items-center md:flex-row gap-8 justify-between">
+                <img
+                    src="/signature.svg"
+                    alt="Signature"
+                    className="invert w-70 h-50"
+                />
+                <img
+                    src="/me.jpg"
+                    alt="Signature"
+                    className="rounded-2xl w-70 h-70 "
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,47 @@
 import { Link } from "react-router";
+import { useState } from "react";
 
 const menuItems = [
-  { id: 4, title: "About", link: "/about" },
-  {
-    id: 1,
-    title: "Projects",
-    link: "/projects",
-  },
-  {
-    id: 2,
-    title: "Blog",
-    link: "/blog",
-  },
-  { id: 3, title: "Contact", link: "/contact" },
+    { id: 4, title: "About", link: "/about" },
+    {
+        id: 1,
+        title: "Projects",
+        link: "/projects",
+    },
+    {
+        id: 2,
+        title: "Blog",
+        link: "/blog",
+    },
+    { id: 3, title: "Contact", link: "/contact" },
 ];
 
 const Header = () => {
-  return (
-    <div className="bg-zinc-900 py-5 px-3 text-white">
-      <div className="max-w-xl mx-auto">
-        <p className="text-center uppercase tracking-widest text-4xl my-3">
-          <Link to="">Sumit Pradhan</Link>
-        </p>
-        <div className="mt-5 flex flex-row gap-3 items-center justify-center">
-          {menuItems.map((item) => (
-            <Link
-              to={item.link}
-              key={item.id}
-              className="rounded bg-zinc-800 px-4 py-2"
-            >
-              {item.title}
-            </Link>
-          ))}
+    const [active, setActive] = useState("");
+
+    return (
+        <div className="bg-zinc-900 py-5 px-3 text-white">
+            <div className="max-w-xl mx-auto">
+                <p className="text-center uppercase tracking-widest text-4xl my-3">
+                    <Link to="" onClick={() => setActive("")}>
+                        Sumit Pradhan
+                    </Link>
+                </p>
+                <div className="mt-5 flex flex-row gap-3 items-center justify-center">
+                    {menuItems.map((item) => (
+                        <Link
+                            to={item.link}
+                            key={item.id}
+                            className={`rounded  px-4 py-2 ${active === item.title ? "bg-orange-700" : "bg-zinc-800"}`}
+                            onClick={() => setActive(item.title)}
+                        >
+                            {item.title}
+                        </Link>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
